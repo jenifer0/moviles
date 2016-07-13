0b83340bc154105e635000128201a9ce1a948d65
jenifer0 <jenifer.vizcarra.0@gmail.com>
2016-07-12 19:56:00 -0500

    Add files via upload

---
+++ iconosMenu.php
@@ -0,0 +1,9 @@
+[ 
+{ "image":"http://i1376.photobucket.com/albums/ah20/Jenifervizcarra0/bt5_zpsa3f6dyno.png", "name":"Tecnología" }, 
+{ "image":"http://i1376.photobucket.com/albums/ah20/Jenifervizcarra0/bt5_zpsa3f6dyno.png", "name":"Accesorios" }, 
+{ "image":"http://i1376.photobucket.com/albums/ah20/Jenifervizcarra0/bt8_zpskpigrc1j.png", "name":"Otros" }, 
+{ "image":"http://i1376.photobucket.com/albums/ah20/Jenifervizcarra0/bt1_zps0uvkif2z.png", "name":"Ropa Dama" }, 
+{ "image":"http://i1376.photobucket.com/albums/ah20/Jenifervizcarra0/bt2_zpshpha4sae.png", "name":"Ropa Hombre" }, 
+{ "image":"http://i1376.photobucket.com/albums/ah20/Jenifervizcarra0/bt3_zpsx3uon1qr.png", "name":"Cuidado Personal" } 
+] 
+
